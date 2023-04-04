1e24eed80a4d2b244b4681c5809b66dc1f7901a8
Pablo Curty <pablocurty@id.uff.br>
2023-04-04 19:50:53 -0300

    add theme centralization

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,33 @@ class MyExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    final ThemeData tema = ThemeData();
+
+    return MaterialApp(
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -117,7 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               width: double.infinity,
               child: const Card(
-                color: Colors.blue,
+                color: Colors.purple,
                 elevation: 5,
                 child: Text('Graphic'),
               ),
